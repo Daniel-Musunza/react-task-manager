4e4086227c00b9c568af660f823a44dba98363b9
Daniel-Musunza <musunzafestus2019@gmail.com>
2023-06-24 11:26:51 +0300

    add time to task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -7,9 +7,10 @@ export const Todo = ({ task, toggleComplete, deleteTodo, editTodo }) => {
         onClick={() => toggleComplete(task.id)}
         className={`${task.completed ? 'completed' : ''}`}
       >
-        {task.task} {task.time}hrs
+       
       </p>
       <div>
+      {task.task} {task.time}hrs
         <i className="fa-solid fa-pen-to-square" onClick={() => editTodo(task.id)}></i>
         <i className="fa-solid fa-trash" onClick={() => deleteTodo(task.id)}></i>
       </div>
